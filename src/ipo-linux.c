83adb2f5c8cea4bcb1f95837811a006fdbd0fa6b
Richard Denton <isdampe@gmail.com>
2017-03-10 12:20:23 +1100

    Couple of logic fixes

--- src/ipo-linux.c
+++ src/ipo-linux.c
@@ -11,7 +11,8 @@ void ipo_usage(void);
 
 int main(int argc, char *argv[])
 {
-	int sockfd, portno, i;
+	unsigned int sockfd, portno, i;
+	unsigned short ipo_loop_error;
 	struct sockaddr_in serv_addr;
 	struct hostent *server;
 
@@ -25,6 +26,8 @@ int main(int argc, char *argv[])
 	i = 2;
 	for ( i; i<argc; i++ ) {
 
+		ipo_loop_error = 0;
+
 		//Set port number
 		portno = atoi(argv[i]);
 
@@ -32,33 +35,37 @@ int main(int argc, char *argv[])
 		sockfd = socket(AF_INET, SOCK_STREAM, 0);
 		if (sockfd < 0)  {
 			fprintf(stderr, "Error creating socket.\n");
-			exit(1);
+			ipo_loop_error = 1;
 		}
 
 		//Get hostname.
 		server = gethostbyname(argv[1]);
 		if (server == NULL) {
 			fprintf(stderr, "Could not resolve host %s.\n", argv[1]);
-			exit(1);
+			ipo_loop_error = 1;
 		}
 
-		//Build the connection.
-		bzero((char *) &serv_addr, sizeof(serv_addr));
-		serv_addr.sin_family = AF_INET;
-		bcopy((char *)server->h_addr, 
-				(char *)&serv_addr.sin_addr.s_addr,
-				server->h_length);
-		serv_addr.sin_port = htons(portno);
+		//If we have a socket or hostname error, skip this.
+		if ( ipo_loop_error != 1 ) {
 
-		if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)  {
-			fprintf(stderr, "Port %i closed.\n", portno);
-			exit(1);
+			//Build the connection.
+			bzero((char *) &serv_addr, sizeof(serv_addr));
+			serv_addr.sin_family = AF_INET;
+			bcopy((char *)server->h_addr, 
+					(char *)&serv_addr.sin_addr.s_addr,
+					server->h_length);
+			serv_addr.sin_port = htons(portno);
+
+			if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)  {
+				fprintf(stderr, "Port %i closed.\n", portno);
+			} else {
+				printf("Port %i is open!\n", portno);
+			}
+
 		}
 
 		//Close the connection
 		close(sockfd);
-
-		printf("Port %i is open!\n", portno);
 
 	}
 	return 0;
